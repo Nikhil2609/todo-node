15aa8473b054ad2baf25de635ee728bed1b8019d
Nikhil Panchal <nikhil.panchal@tatvasoft.com>
2025-09-09 10:42:25 +0530

    changes

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -17,5 +17,8 @@ npm install
 DATABASE_URL=$(aws ssm get-parameter --name "/todo-app/$ENVIRONMENT/DATABASE_URL" --with-decryption --query "Parameter.Value" --output text)
 export DATABASE_URL=$DATABASE_URL
 
+echo "Generating Prisma client..."
+npx prisma generate
+
 echo "Generate Prisma Client"
 npx prisma migrate deploy
