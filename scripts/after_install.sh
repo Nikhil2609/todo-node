a8c89a77c1d70a346d9d04c85fa304dc505fba1c
Nikhil Panchal <nikhil.panchal@tatvasoft.com>
2025-09-12 09:51:35 +0530

    builspec changes

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,24 +1,8 @@
 echo "after install script"
 cd /home/ec2-user/project-api
 
-# Fetch Environment Variables
-# Load ENV variable from the artifact
-
-if [ -f envfile ]; then
-  export $(cat envfile | xargs)
-fi
-
-echo "Using ENVIRONMENT=$ENVIRONMENT"
-
 echo "Install Dependencies"
 npm install
-
-
-DATABASE_URL=$(aws ssm get-parameter --name "/todo-app/$ENVIRONMENT/DATABASE_URL" --with-decryption --query "Parameter.Value" --output text)
-export DATABASE_URL=$DATABASE_URL
-
-# create env variable
-echo "DATABASE_URL=$DATABASE_URL" > .env
 
 # generate prisma client
 echo "Generate Prisma Client"
