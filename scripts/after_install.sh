c75e4964f2be812093000eebd7efe35b3c6d6166
Nikhil Panchal <nikhil.panchal@tatvasoft.com>
2025-09-12 14:23:23 +0530

    quick fixes

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,9 @@
 echo "after install script"
 cd /home/ec2-user/project-api
 
+echo "ec2-user as owner of project-api"
+sudo chown -R ec2-user:ec2-user /home/ec2-user/project-api
+
 echo "Install Dependencies"
 npm install
 
