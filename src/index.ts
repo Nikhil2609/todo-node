ffdd78e4defeffd4d20f4d5d5868c9b47d5ec93e
Nikhil Panchal <nikhil.panchal@tatvasoft.com>
2025-10-15 11:51:00 +0530

    Implement Socket functionality

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import dotenv from 'dotenv';
 import { requestLogger } from './middleware/logger';
 import { errorHandler } from './middleware/errorHandler';
 import cors from 'cors';
+import http from 'http';
+import { Server } from 'socket.io';
 
 dotenv.config();
 
@@ -13,6 +15,27 @@ export const app = express();
 const corsOptions = {
   origin: ['http://localhost:3002', 'http://localhost:5173', 'http://d3uky4au1tedcv.cloudfront.net']
 };
+
+const server = http.createServer(app);
+
+// socket connection
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173'
+  }
+});
+io.on('connection', (socket) => {
+  console.log('a user connected on ', socket.id);
+  socket.on('updateTask', () => { // on : listen event emit from client
+    console.log('BE: updateTask listen');
+
+    io.emit('updateBoard');  // emit : send this event to client and then client update board in all browser window
+    console.log('BE: updateBoard emit');
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
 
 // middleware
 app.use(cors(corsOptions)); // allow api access to valid FE URL only
@@ -38,6 +61,10 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+//   console.log('server running on port', PORT);
+// });
+
+server.listen(PORT, () => {
   console.log('server running on port', PORT);
 });
--- src/routes/todo.routes.ts
+++ src/routes/todo.routes.ts
@@ -4,35 +4,34 @@ import prisma from '../prisma';
 const todoRouter = express.Router();
 
 // Get all todos
-todoRouter.get("", async (req, res) => {
-  const todos = await prisma.todo.findMany({ orderBy: { id: "desc" } });
+todoRouter.get('', async (req, res) => {
+  const todos = await prisma.todo.findMany({ orderBy: { id: 'desc' } });
   return res.json(todos);
 });
 
 // Add todo
-todoRouter.post("", async (req, res) => {
+todoRouter.post('', async (req, res) => {
   const { title } = req.body;
   const todo = await prisma.todo.create({ data: { title } });
   res.json(todo);
 });
 
 // Update todo
-todoRouter.put("/:id", async (req, res) => {
+todoRouter.put('/:id', async (req, res) => {
   const { id } = req.params;
   const { title, completed } = req.body;
   const todo = await prisma.todo.update({
     where: { id: Number(id) },
-    data: { title, completed },
+    data: { title, completed }
   });
   res.json(todo);
 });
 
 // Delete todo
-todoRouter.delete("/:id", async (req, res) => {
+todoRouter.delete('/:id', async (req, res) => {
   const { id } = req.params;
   await prisma.todo.delete({ where: { id: Number(id) } });
-  res.json({ message: "Todo deleted" });
+  res.json({ message: 'Todo deleted' });
 });
-
 
 export default todoRouter;
